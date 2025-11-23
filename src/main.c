b6e7ce820087e2254b3b5a72ab9a41c36d566058
Elvis Oliveira <elvis.olv@gmail.com>
2025-11-23 19:25:07 +0100

    Comment out BT device name config and standardize code formatting

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 
-#define DEVICE_NAME             CONFIG_BT_DEVICE_NAME
-#define DEVICE_NAME_LEN         (sizeof(DEVICE_NAME) - 1)
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -22,24 +22,24 @@ static const struct bt_data ad[] = {
 
 void main(void)
 {
-  int err = 0;
+	int err = 0;
 	printk("Change this to see it change.\n");
 
 	err = bt_enable(NULL);
-	if (err) {
+	if (err)
+	{
 		printk("Bluetooth init failed (err %d)\n", err);
 		return;
 	}
 
 	printk("Bluetooth initialized\n");
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad),
-			      NULL, 0);
-	if (err) {
+	err = bt_le_adv_start(BT_LE_ADV_CONN, ad, ARRAY_SIZE(ad), NULL, 0);
+	if (err)
+	{
 		printk("Advertising failed to start (err %d)\n", err);
 		return;
 	}
 
 	printk("Advertising successfully started\n");
-
 }
